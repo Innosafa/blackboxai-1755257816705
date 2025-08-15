bf32ac38d16daa8b60d8e038dbb0ca19e3b12e42
Innosafa <165649910+Innosafa@users.noreply.github.com>
2025-08-15 14:37:33 +0300

    Add .next/static/chunks/src_app_results_page_tsx_87126907._.js 1755257852212

---
+++ .next/static/chunks/src_app_results_page_tsx_87126907._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_8acd3a29._.js"
+],
+    source: "dynamic"
+});
